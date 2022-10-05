df8abbdf4b36f17dc32c917b74acc98a54d1e6ee
Mikael Sloth <mikael.sloth@tnt.dk>
2022-10-05 16:18:47 +0200

    Adding Test project for shipment object class

---
+++ TNTExpressConnectShipment/SerializeExtensionMethods.cs
@@ -0,0 +1,44 @@
+﻿namespace TNTExpressConnectShipment
+{
+    using System;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Serialization;
+
+    public static class SerializeExtensionMethods
+    {
+        public static T? DeserializeFromXDoc<T>(this XDocument source) where T : ShipmentRequest
+        {
+            if (source is null || source.Root is null)
+                return default;
+
+            try
+            {
+                using var reader = source.Root.CreateReader();
+                return new XmlSerializer(typeof(T)).Deserialize(reader) is T t ? t : null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static XDocument? SerializeToXDoc<T>(this T source) where T : ShipmentRequest
+        {
+            if (source is null)
+                return null;
+            try
+            {
+                XDocument doc = new(new XDeclaration("1.0", "utf-8", "yes"));
+                using XmlWriter writer = doc.CreateWriter();
+                new XmlSerializer(typeof(T)).Serialize(writer, source);
+
+                return doc;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ TNTExpressConnectShipmentTests/SerializeExtensionMethodsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace TNTExpressConnectShipment.Tests
+{
+    using Xunit;
+    using TNTExpressConnectShipment;
+
+    public class SerializeExtensionMethodsTests
+    {
+        [Fact()]
+        public void DeserializeFromXDocTest()
+        {
+
+        }
+
+        [Fact()]
+        public void SerializeToXDocTest()
+        {
+
+        }
+    }
+}
